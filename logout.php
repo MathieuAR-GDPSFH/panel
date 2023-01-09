0385ed51a087e23ad1853ce6a1b218501a05b729
Jecket22 <lez.vezzica@gmail.com>
2023-01-09 13:18:41 +0100

    lazily add logout button

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+// a bit overkill, but oh well, this works...
+session_start();
+session_unset();
+session_destroy();
+session_write_close();
+setcookie(session_name(),'',0,'/');
+session_regenerate_id(true);
+
+header('Location: https://panel.fhgdps.com/');
+die();
+?>
